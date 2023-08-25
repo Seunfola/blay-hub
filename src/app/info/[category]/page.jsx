e0f4b473b069015ded16867849ce313a3bca0617
Folahan mosunmola <folahan.mosunmola@thebulbafrica.institute>
2023-08-25 18:46:37 +0100

    fixed the button component

--- src/app/info/[category]/page.jsx
+++ src/app/info/[category]/page.jsx
@@ -26,7 +26,7 @@ const Category = ({ params }) => {
           <div className={styles.content}>
             <h1 className={styles.title}>{item.title}</h1>
             <p className={styles.desc}>{item.desc}</p>
-            <Button text="See More" url="#" />
+            {/* <Button text="See More" url="#" /> */}
           </div>
           <div className={styles.imgContainer}>
             <Image
