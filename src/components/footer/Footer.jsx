f3d9b55046959cdc3f768d1ab359c6e388116270
Folahan mosunmola <seunfola1@gmail.com>
2023-09-03 04:55:23 +0100

    footer fitted

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div> @2023 Blay-hub Consultancy. All rights reserved.</div>
+      <div className={styles.text}> @2023 Blay-hub Consultancy. All rights reserved.</div>
       <div className={styles.social}>
         <Image src="/1.png" width={15} height={15} className={styles.icon} alt='facebook' />
         <Image src="/2.png" width={15} height={15} className={styles.icon} alt='insta' />
