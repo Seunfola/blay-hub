d1c653868e32ba6f1d68ae1de8562077836e08bd
Folahan mosunmola <seunfola1@gmail.com>
2023-09-07 18:07:08 +0100

    updated the send for the contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,22 +4,40 @@ import Image from 'next/image';
 import Button from '@/components/button/Button';
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_1gmvoew', 'template_ao08965', form.current, '_8GeNeGxFow4XwMnF')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
+  const notify = () => {
+    toast('Message Sent and delivered successfully.');
+  }
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}> Let's keep in Touch'</h1>
       <div className={styles.content}>
         <div className={styles.imgContainer}>
-          <Image src="/contact.png" 
-          className={styles.image}
-          fill={true}
+          <Image src="/contact.png"
+            className={styles.image}
+            fill={true}
 
-          alt='contact pic'/>
+            alt='contact pic' />
         </div>
-        <div className={styles.form}>
-          <input type='text' placeholder='name' className={styles.input} />
-          <input type='text' placeholder='email' className={styles.input} />
-          <textarea className={styles.textArea} placeholder="Message" cols="30" rows="10"></textarea>
-          <Button text="send" url="#"/>
+        <div className={styles.form} ref={form} onSubmit={sendEmail}>
+          <input type='text' placeholder='name' id='name' name='name' className={styles.input} required />
+          <input type='text' placeholder='email' id='email' name='email' className={styles.input} required />
+          <textarea className={styles.textArea} placeholder="Message" cols="30" rows="10" autoComplete='on' spellCheck='true' required ></textarea>
+          <Button text="send" url="#" value='Send' onClick={notify} />
         </div>
       </div>
     </div>
